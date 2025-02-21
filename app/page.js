db5763be98695ffc0205a08923f75719bade0738
fcansi04 <ferhatcansi.04@outlook.com>
2025-02-21 06:54:49 +0300

    some needless parts removed

--- app/page.js
+++ app/page.js
@@ -1,15 +1,8 @@
-import Image from "next/image";
 import Feed from "@/Components/Feed";
-import Head from "next/head";
+
 export default function Home() {
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&"
-        />
-      </Head>
       <Feed />
     </>
   );
