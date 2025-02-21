684139bed8583f39273d738c7ca0f4abff340db5
fcansi04 <ferhatcansi.04@outlook.com>
2025-02-21 07:41:19 +0300

    config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,11 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
